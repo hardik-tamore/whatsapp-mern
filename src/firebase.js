3681cddb382eba68b9960395fb0e0c5582720b32
hardik-tamore <hardiktamore.ht@gmail.com>
2020-09-20 20:46:19 +0530

    getting started

---
+++ src/firebase.js
@@ -0,0 +1,9 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCn55kFTJcBLEnPAAM9BX9pwbsN92bArUs",
+  authDomain: "whatsapp-mern-81ce4.firebaseapp.com",
+  databaseURL: "https://whatsapp-mern-81ce4.firebaseio.com",
+  projectId: "whatsapp-mern-81ce4",
+  storageBucket: "whatsapp-mern-81ce4.appspot.com",
+  messagingSenderId: "445919722215",
+  appId: "1:445919722215:web:2b2407b1407155c4c48042",
+};
